75288b1484b91f271f5ccaad91b3ab00347587d0
Gordon Murray <gordon@gordonmurray.com>
2023-02-06 18:40:02 +0000

    Checkly API checks

---
+++ checkly_api.tf
@@ -0,0 +1,66 @@
+resource "checkly_check" "webserver_check" {
+  name                      = "Webserver check"
+  type                      = "API"
+  activated                 = true
+  should_fail               = false
+  frequency                 = 15
+  double_check              = true
+  use_global_alert_settings = true
+
+  locations = [
+    "eu-west-1",
+    "eu-west-2"
+  ]
+
+  tags = ["aws", "terraform"]
+
+  request {
+    url              = "http://${aws_instance.webserver.public_dns}"
+    follow_redirects = true
+    body_type        = "JSON"
+    method           = "GET"
+
+    assertion {
+      source     = "STATUS_CODE"
+      comparison = "EQUALS"
+      target     = "200"
+    }
+  }
+}
+
+resource "checkly_check" "sendgrid_check" {
+  name                      = "Sendgrid Incident check"
+  type                      = "API"
+  activated                 = true
+  should_fail               = false
+  frequency                 = 15
+  double_check              = true
+  use_global_alert_settings = true
+
+  locations = [
+    "eu-west-1",
+    "eu-west-2"
+  ]
+
+  tags = ["aws", "terraform", "sendgrid"]
+
+  request {
+    url              = "https://status.sendgrid.com/api/v2/summary.json"
+    follow_redirects = true
+    body_type        = "JSON"
+    method           = "GET"
+
+    assertion {
+      source     = "STATUS_CODE"
+      comparison = "EQUALS"
+      target     = "200"
+    }
+
+    assertion {
+      source     = "JSON_BODY"
+      property   = "$.incidents"
+      comparison = "IS_EMPTY"
+      target     = ""
+    }
+  }
+}
